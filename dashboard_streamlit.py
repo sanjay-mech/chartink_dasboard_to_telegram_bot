70c47396fb782b089f4b5453a1b27a4d5b318e75
Sanjay Jangid <64764726+sanjay-mech@users.noreply.github.com>
2023-09-21 00:16:33 +0530

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -1,41 +1,36 @@
 import streamlit as st
 import subprocess
-import time
 
 # Streamlit app title
-st.title("Run Python Script")
+st.title("Continuous Python Script Runner")
 
 # Define the Python script file name (modify this)
 script_file = "final_Dashboard_to_telegram.py"  # Modify this line with your script file name
 
-# Flag to track whether the script is running
-script_running = False
+# Function to run the script and capture its output
+def run_script():
+    try:
+        # Run the script using subprocess
+        result = subprocess.Popen(["python", script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
-try:
-    # Run the script using subprocess
-    result = subprocess.Popen(["python", script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        # Print a message to indicate that the script is running
+        st.subheader("Script Output:")
+        st.info("Python script is running...")
 
-    # Set the flag to indicate that the script is running
-    script_running = True
+        # Continuously display the output of the script
+        while True:
+            output = result.stdout.readline()
+            if output == '' and result.poll() is not None:
+                break
+            if output:
+                st.code(output, language="python")
 
-    # Print a message to indicate that the script is running
-    st.subheader("Script Output:")
-    st.info("Python script is running...")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
-    # Check the script's status and print the output
-    while script_running:
-        output = result.stdout.readline()
-        if output == '' and result.poll() is not None:
-            break
-        if output:
-            st.code(output, language="python")
-        time.sleep(0.1)
+# Run the script continuously
+while True:
+    run_script()
 
-    # Print a message when the script has finished running
-    st.success("Python script has finished running.")
-
-except Exception as e:
-    st.error(f"An error occurred: {e}")
-
-# Footer
+# Footer (this will never be reached as the script runs continuously)
 st.text("Note: Modify 'script_file' with the name of your Python script.")
